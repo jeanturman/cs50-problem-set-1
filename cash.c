d131d8976df68d6bb1f1057283c363658b78af9d
jeanturman <45578056+jeanturman@users.noreply.github.com>
2019-01-16 13:04:25 -0600

    Create cash.c
    program to determine the least amount of coins to return the proper amount of change.

---
+++ cash.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    
+    float amount = get_float("How much change are you owed? ");
+    printf("You will receive %f in change as follows: \n", amount);
+        
+    
+}
