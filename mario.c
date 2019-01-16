156fc7a8a4b5330624b5c1b6e38ab2b6d495a4f4
jeanturman <45578056+jeanturman@users.noreply.github.com>
2019-01-16 12:38:00 -0600

    Update mario.c

--- mario.c
+++ mario.c
@@ -3,12 +3,14 @@
 
 int main(void) {
     int height;
-    
+
+    // continue prompting until the condition (positive integer less than 21) is met
     do {
         height = get_int("Please enter the desired height of structure, no greter than 20: ");
     } 
         while (height > 21 || height < 0);
     
+    // draw the structure incrementally
     for(int i = height; i >= 1; i--) {
         for(int j = 0; j <= height; j++)
             if(j >= i - 1)
