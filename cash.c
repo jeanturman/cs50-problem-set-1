0459f4eb95495aed13526be2522a617026977ae3
jeanturman <45578056+jeanturman@users.noreply.github.com>
2019-01-16 15:49:48 -0600

    Update cash.c

--- cash.c
+++ cash.c
@@ -3,9 +3,15 @@
 
 int main(void)
 {
+    float itemCost = get_float("How much does the item cost? ");
+    float moneyGiven = get_float("How much cash will you give for the item? ");
+    float changeNeeded = moneyGiven - itemCost;
     
-    float amount = get_float("How much change are you owed? ");
-    printf("You will receive %f in change as follows: \n", amount);
-        
+   while (changeNeeded < 0) {
+       printf("That's not enough money. How much will you give? ");
+       moneyGiven = get_float();
+       
+    } 
     
+    printf("You will receive %f in change as follows: \n", changeNeeded);         
 }
